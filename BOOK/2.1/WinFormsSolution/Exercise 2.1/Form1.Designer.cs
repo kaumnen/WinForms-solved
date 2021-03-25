ae2a9112aa8fa1e662b3699312692f481c05f19b
Aleksej Komnenovic <main@komnen.xyz>
2021-03-25 10:27:28 +0100

    set up design

--- BOOK/2.1/WinFormsSolution/Exercise 2.1/Form1.Designer.cs
+++ BOOK/2.1/WinFormsSolution/Exercise 2.1/Form1.Designer.cs
@@ -30,12 +30,179 @@ namespace Exercise_2._1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.NameSurnameTextBox = new System.Windows.Forms.TextBox();
+            this.AddressTextBox = new System.Windows.Forms.TextBox();
+            this.CourseTextBox = new System.Windows.Forms.TextBox();
+            this.AgeTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.IzlazTextBox = new System.Windows.Forms.Label();
+            this.GitHubLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.OKButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameSurnameTextBox
+            // 
+            this.NameSurnameTextBox.Location = new System.Drawing.Point(134, 41);
+            this.NameSurnameTextBox.Name = "NameSurnameTextBox";
+            this.NameSurnameTextBox.Size = new System.Drawing.Size(243, 20);
+            this.NameSurnameTextBox.TabIndex = 0;
+            // 
+            // AddressTextBox
+            // 
+            this.AddressTextBox.Location = new System.Drawing.Point(134, 78);
+            this.AddressTextBox.Multiline = true;
+            this.AddressTextBox.Name = "AddressTextBox";
+            this.AddressTextBox.Size = new System.Drawing.Size(243, 84);
+            this.AddressTextBox.TabIndex = 1;
+            // 
+            // CourseTextBox
+            // 
+            this.CourseTextBox.Location = new System.Drawing.Point(134, 181);
+            this.CourseTextBox.Name = "CourseTextBox";
+            this.CourseTextBox.Size = new System.Drawing.Size(243, 20);
+            this.CourseTextBox.TabIndex = 2;
+            // 
+            // AgeTextbox
+            // 
+            this.AgeTextbox.Location = new System.Drawing.Point(134, 214);
+            this.AgeTextbox.Name = "AgeTextbox";
+            this.AgeTextbox.Size = new System.Drawing.Size(44, 20);
+            this.AgeTextbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name Surname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Address:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Course:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Age:";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(41, 304);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(383, 86);
+            this.OutputTextBox.TabIndex = 8;
+            // 
+            // IzlazTextBox
+            // 
+            this.IzlazTextBox.AutoSize = true;
+            this.IzlazTextBox.Location = new System.Drawing.Point(38, 273);
+            this.IzlazTextBox.Name = "IzlazTextBox";
+            this.IzlazTextBox.Size = new System.Drawing.Size(39, 13);
+            this.IzlazTextBox.TabIndex = 9;
+            this.IzlazTextBox.Text = "Output";
+            // 
+            // GitHubLinkLabel
+            // 
+            this.GitHubLinkLabel.AutoSize = true;
+            this.GitHubLinkLabel.Location = new System.Drawing.Point(343, 340);
+            this.GitHubLinkLabel.Name = "GitHubLinkLabel";
+            this.GitHubLinkLabel.Size = new System.Drawing.Size(40, 13);
+            this.GitHubLinkLabel.TabIndex = 10;
+            this.GitHubLinkLabel.TabStop = true;
+            this.GitHubLinkLabel.Text = "GitHub";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(451, 39);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 11;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(451, 212);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 12;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(561, 402);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.GitHubLinkLabel);
+            this.Controls.Add(this.IzlazTextBox);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AgeTextbox);
+            this.Controls.Add(this.CourseTextBox);
+            this.Controls.Add(this.AddressTextBox);
+            this.Controls.Add(this.NameSurnameTextBox);
+            this.Name = "Form1";
+            this.Text = "Student";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox NameSurnameTextBox;
+        private System.Windows.Forms.TextBox AddressTextBox;
+        private System.Windows.Forms.TextBox CourseTextBox;
+        private System.Windows.Forms.TextBox AgeTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Label IzlazTextBox;
+        private System.Windows.Forms.LinkLabel GitHubLinkLabel;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button ExitButton;
     }
 }
 
