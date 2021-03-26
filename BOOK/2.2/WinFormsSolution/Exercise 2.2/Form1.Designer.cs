578564076814ce71848a220e9b5cb7801a18c87b
Aleksej Komnenovic <main@komnen.xyz>
2021-03-26 22:15:09 +0100

    set up design with custom names for each tool

--- BOOK/2.2/WinFormsSolution/Exercise 2.2/Form1.Designer.cs
+++ BOOK/2.2/WinFormsSolution/Exercise 2.2/Form1.Designer.cs
@@ -29,13 +29,68 @@ namespace Exercise_2._2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ContentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ContentRichTextBox
+            // 
+            this.ContentRichTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ContentRichTextBox.Name = "ContentRichTextBox";
+            this.ContentRichTextBox.Size = new System.Drawing.Size(380, 205);
+            this.ContentRichTextBox.TabIndex = 0;
+            this.ContentRichTextBox.Text = "";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(39, 243);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(39, 296);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(39, 349);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 394);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ContentRichTextBox);
+            this.Name = "Form1";
+            this.Text = "RichTextBox";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox ContentRichTextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button ClearButton;
     }
 }
 
