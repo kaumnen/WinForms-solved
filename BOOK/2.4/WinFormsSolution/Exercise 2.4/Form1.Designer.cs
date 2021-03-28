fb38eb820b962973afdccf2f969fe98e6df18395
Aleksej Komnenovic <main@komnen.xyz>
2021-03-28 20:11:42 +0200

    set up logic

--- BOOK/2.4/WinFormsSolution/Exercise 2.4/Form1.Designer.cs
+++ BOOK/2.4/WinFormsSolution/Exercise 2.4/Form1.Designer.cs
@@ -29,33 +29,92 @@ namespace Exercise_2._4
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TransportationTypeGroupBox = new System.Windows.Forms.GroupBox();
+            this.PerTransRadioButton = new System.Windows.Forms.RadioButton();
+            this.BusRadioButton = new System.Windows.Forms.RadioButton();
+            this.PlaneRadioButton = new System.Windows.Forms.RadioButton();
+            this.ChoiceTextBox = new System.Windows.Forms.TextBox();
+            this.TransportationTypeGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // TransportationTypeGroupBox
             // 
-            this.groupBox1.Location = new System.Drawing.Point(90, 45);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.TransportationTypeGroupBox.Controls.Add(this.PerTransRadioButton);
+            this.TransportationTypeGroupBox.Controls.Add(this.BusRadioButton);
+            this.TransportationTypeGroupBox.Controls.Add(this.PlaneRadioButton);
+            this.TransportationTypeGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.TransportationTypeGroupBox.Name = "TransportationTypeGroupBox";
+            this.TransportationTypeGroupBox.Size = new System.Drawing.Size(160, 113);
+            this.TransportationTypeGroupBox.TabIndex = 0;
+            this.TransportationTypeGroupBox.TabStop = false;
+            this.TransportationTypeGroupBox.Text = "Transportation type";
+            // 
+            // PerTransRadioButton
+            // 
+            this.PerTransRadioButton.AutoSize = true;
+            this.PerTransRadioButton.Location = new System.Drawing.Point(24, 81);
+            this.PerTransRadioButton.Name = "PerTransRadioButton";
+            this.PerTransRadioButton.Size = new System.Drawing.Size(114, 17);
+            this.PerTransRadioButton.TabIndex = 2;
+            this.PerTransRadioButton.TabStop = true;
+            this.PerTransRadioButton.Text = "Personal Transport";
+            this.PerTransRadioButton.UseVisualStyleBackColor = true;
+            this.PerTransRadioButton.Click += new System.EventHandler(this.TransportationRadioButtonClickedUniversal_CheckedChanged);
+            // 
+            // BusRadioButton
+            // 
+            this.BusRadioButton.AutoSize = true;
+            this.BusRadioButton.Location = new System.Drawing.Point(24, 57);
+            this.BusRadioButton.Name = "BusRadioButton";
+            this.BusRadioButton.Size = new System.Drawing.Size(43, 17);
+            this.BusRadioButton.TabIndex = 1;
+            this.BusRadioButton.TabStop = true;
+            this.BusRadioButton.Text = "Bus";
+            this.BusRadioButton.UseVisualStyleBackColor = true;
+            this.BusRadioButton.Click += new System.EventHandler(this.TransportationRadioButtonClickedUniversal_CheckedChanged);
+            // 
+            // PlaneRadioButton
+            // 
+            this.PlaneRadioButton.AutoSize = true;
+            this.PlaneRadioButton.Location = new System.Drawing.Point(24, 33);
+            this.PlaneRadioButton.Name = "PlaneRadioButton";
+            this.PlaneRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.PlaneRadioButton.TabIndex = 0;
+            this.PlaneRadioButton.TabStop = true;
+            this.PlaneRadioButton.Text = "Plane";
+            this.PlaneRadioButton.UseVisualStyleBackColor = true;
+            this.PlaneRadioButton.Click += new System.EventHandler(this.TransportationRadioButtonClickedUniversal_CheckedChanged);
+            // 
+            // ChoiceTextBox
+            // 
+            this.ChoiceTextBox.Location = new System.Drawing.Point(24, 150);
+            this.ChoiceTextBox.Name = "ChoiceTextBox";
+            this.ChoiceTextBox.Size = new System.Drawing.Size(296, 20);
+            this.ChoiceTextBox.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(332, 198);
+            this.Controls.Add(this.ChoiceTextBox);
+            this.Controls.Add(this.TransportationTypeGroupBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Travel";
+            this.TransportationTypeGroupBox.ResumeLayout(false);
+            this.TransportationTypeGroupBox.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox TransportationTypeGroupBox;
+        private System.Windows.Forms.RadioButton PerTransRadioButton;
+        private System.Windows.Forms.RadioButton BusRadioButton;
+        private System.Windows.Forms.RadioButton PlaneRadioButton;
+        private System.Windows.Forms.TextBox ChoiceTextBox;
     }
 }
 
--- BOOK/2.4/WinFormsSolution/Exercise 2.4/Form1.cs
+++ BOOK/2.4/WinFormsSolution/Exercise 2.4/Form1.cs
@@ -15,6 +15,18 @@ namespace Exercise_2._4
         public Form1()
         {
             InitializeComponent();
+
+            PlaneRadioButton.Checked = true;
+        }
+
+        private void WriteToTextBox(RadioButton rb)
+        {
+            ChoiceTextBox.Text = $"You have chosen {rb.Text} transportation type!";
+        }
+
+        private void TransportationRadioButtonClickedUniversal_CheckedChanged(object sender, EventArgs e)
+        {
+            WriteToTextBox((RadioButton)sender);
         }
     }
 }
