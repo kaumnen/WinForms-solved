8c46f65032752cf4c90b5eeb2fe38eb1da96128b
Aleksej Komnenovic <main@komnen.xyz>
2021-04-06 00:21:11 +0200

    set up data model to transfer data from one form to another

---
+++ BOOK/2.6/WinFormsSolution/Exercise 2.6/DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2._6
+{
+    class DataModel
+    {
+        public static string Data { get; set; }
+    }
+}
