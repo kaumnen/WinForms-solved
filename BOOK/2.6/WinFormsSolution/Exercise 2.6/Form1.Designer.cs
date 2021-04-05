49f6782f4b3df26c4205c42093d225afd73b52cb
Aleksej Komnenovic <main@komnen.xyz>
2021-04-06 00:11:30 +0200

    set up design for starting form

--- BOOK/2.6/WinFormsSolution/Exercise 2.6/Form1.Designer.cs
+++ BOOK/2.6/WinFormsSolution/Exercise 2.6/Form1.Designer.cs
@@ -29,13 +29,44 @@ namespace Exercise_2._6
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StartingTextBox = new System.Windows.Forms.TextBox();
+            this.OpenNewFormButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartingTextBox
+            // 
+            this.StartingTextBox.Location = new System.Drawing.Point(40, 59);
+            this.StartingTextBox.Name = "StartingTextBox";
+            this.StartingTextBox.Size = new System.Drawing.Size(331, 20);
+            this.StartingTextBox.TabIndex = 0;
+            // 
+            // OpenNewFormButton
+            // 
+            this.OpenNewFormButton.Location = new System.Drawing.Point(149, 106);
+            this.OpenNewFormButton.Name = "OpenNewFormButton";
+            this.OpenNewFormButton.Size = new System.Drawing.Size(98, 23);
+            this.OpenNewFormButton.TabIndex = 1;
+            this.OpenNewFormButton.Text = "Open new form";
+            this.OpenNewFormButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(415, 172);
+            this.Controls.Add(this.OpenNewFormButton);
+            this.Controls.Add(this.StartingTextBox);
+            this.Name = "Form1";
+            this.Text = "Data transfer - starting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox StartingTextBox;
+        private System.Windows.Forms.Button OpenNewFormButton;
     }
 }
 
