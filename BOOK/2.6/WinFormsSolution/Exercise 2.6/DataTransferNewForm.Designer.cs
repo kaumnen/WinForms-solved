c0bb55a510dfec6a634b400e480e1fa6dd71058e
Aleksej Komnenovic <main@komnen.xyz>
2021-04-06 00:15:15 +0200

    set up second form design

---
+++ BOOK/2.6/WinFormsSolution/Exercise 2.6/DataTransferNewForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Exercise_2._6
+{
+    partial class DataTransferNewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newFormDataTextBox = new System.Windows.Forms.TextBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newFormDataTextBox
+            // 
+            this.newFormDataTextBox.Location = new System.Drawing.Point(23, 44);
+            this.newFormDataTextBox.Name = "newFormDataTextBox";
+            this.newFormDataTextBox.Size = new System.Drawing.Size(298, 20);
+            this.newFormDataTextBox.TabIndex = 0;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(40, 120);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 1;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(224, 120);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // DataTransferNewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 163);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.newFormDataTextBox);
+            this.Name = "DataTransferNewForm";
+            this.Text = "DataTransferNewForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox newFormDataTextBox;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
