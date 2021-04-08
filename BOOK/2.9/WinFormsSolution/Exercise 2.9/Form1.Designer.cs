02ae9eb3cc8b0325bb0cbdb6c8d085cafc9109b3
Aleksej Komnenovic <main@komnen.xyz>
2021-04-08 22:45:30 +0200

    set up logic for bold button

--- BOOK/2.9/WinFormsSolution/Exercise 2.9/Form1.Designer.cs
+++ BOOK/2.9/WinFormsSolution/Exercise 2.9/Form1.Designer.cs
@@ -29,20 +29,128 @@ namespace Exercise_2._9
         /// </summary>
         private void InitializeComponent()
         {
+            this.TextRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.BoldButton = new System.Windows.Forms.Button();
+            this.ItalicButton = new System.Windows.Forms.Button();
+            this.UnderlineButton = new System.Windows.Forms.Button();
+            this.CenterButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SizeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // TextRichTextBox
+            // 
+            this.TextRichTextBox.Location = new System.Drawing.Point(12, 74);
+            this.TextRichTextBox.Name = "TextRichTextBox";
+            this.TextRichTextBox.Size = new System.Drawing.Size(544, 209);
+            this.TextRichTextBox.TabIndex = 0;
+            this.TextRichTextBox.Text = "";
+            // 
+            // BoldButton
+            // 
+            this.BoldButton.Location = new System.Drawing.Point(57, 13);
+            this.BoldButton.Name = "BoldButton";
+            this.BoldButton.Size = new System.Drawing.Size(75, 23);
+            this.BoldButton.TabIndex = 1;
+            this.BoldButton.Text = "Bold";
+            this.BoldButton.UseVisualStyleBackColor = true;
+            this.BoldButton.Click += new System.EventHandler(this.BoldButton_Click);
+            // 
+            // ItalicButton
+            // 
+            this.ItalicButton.Location = new System.Drawing.Point(176, 13);
+            this.ItalicButton.Name = "ItalicButton";
+            this.ItalicButton.Size = new System.Drawing.Size(75, 23);
+            this.ItalicButton.TabIndex = 2;
+            this.ItalicButton.Text = "Italic";
+            this.ItalicButton.UseVisualStyleBackColor = true;
+            // 
+            // UnderlineButton
+            // 
+            this.UnderlineButton.Location = new System.Drawing.Point(296, 13);
+            this.UnderlineButton.Name = "UnderlineButton";
+            this.UnderlineButton.Size = new System.Drawing.Size(75, 23);
+            this.UnderlineButton.TabIndex = 3;
+            this.UnderlineButton.Text = "Underline";
+            this.UnderlineButton.UseVisualStyleBackColor = true;
+            // 
+            // CenterButton
+            // 
+            this.CenterButton.Location = new System.Drawing.Point(414, 13);
+            this.CenterButton.Name = "CenterButton";
+            this.CenterButton.Size = new System.Drawing.Size(75, 23);
+            this.CenterButton.TabIndex = 4;
+            this.CenterButton.Text = "Center";
+            this.CenterButton.UseVisualStyleBackColor = true;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(57, 289);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 5;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(414, 289);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 6;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // SizeTextBox
+            // 
+            this.SizeTextBox.Location = new System.Drawing.Point(334, 48);
+            this.SizeTextBox.Name = "SizeTextBox";
+            this.SizeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SizeTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Size";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(570, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SizeTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.CenterButton);
+            this.Controls.Add(this.UnderlineButton);
+            this.Controls.Add(this.ItalicButton);
+            this.Controls.Add(this.BoldButton);
+            this.Controls.Add(this.TextRichTextBox);
             this.Name = "Form1";
             this.Text = "Rich text box control";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox TextRichTextBox;
+        private System.Windows.Forms.Button BoldButton;
+        private System.Windows.Forms.Button ItalicButton;
+        private System.Windows.Forms.Button UnderlineButton;
+        private System.Windows.Forms.Button CenterButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox SizeTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- BOOK/2.9/WinFormsSolution/Exercise 2.9/Form1.cs
+++ BOOK/2.9/WinFormsSolution/Exercise 2.9/Form1.cs
@@ -16,5 +16,26 @@ namespace Exercise_2._9
         {
             InitializeComponent();
         }
+
+        private void BoldButton_Click(object sender, EventArgs e)
+        {
+            Font newFont;
+            Font oldFont;
+
+            oldFont = TextRichTextBox.SelectionFont;
+
+            if (oldFont.Bold)
+            {
+                newFont = new Font(oldFont, oldFont.Style & ~FontStyle.Bold);
+            }
+            else
+            {
+                newFont = new Font(oldFont, oldFont.Style | FontStyle.Bold);
+            }
+
+            //setting new font and focus on text area
+            TextRichTextBox.SelectionFont = newFont;
+            TextRichTextBox.Focus();
+        }
     }
 }
