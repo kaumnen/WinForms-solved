7d26d146da303566d7080004d9db2ee76736617e
Aleksej Komnenovic <main@komnen.xyz>
2021-04-11 22:48:52 +0200

    set up initial form design, names and text for each control

--- BOOK/4.1/WinFormsSolution/Exercise 4.1/Form1.Designer.cs
+++ BOOK/4.1/WinFormsSolution/Exercise 4.1/Form1.Designer.cs
@@ -29,13 +29,80 @@ namespace Exercise_4._1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GuestsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.AddGuestButton = new System.Windows.Forms.Button();
+            this.RejectButton = new System.Windows.Forms.Button();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GuestsRichTextBox
+            // 
+            this.GuestsRichTextBox.Location = new System.Drawing.Point(12, 12);
+            this.GuestsRichTextBox.Name = "GuestsRichTextBox";
+            this.GuestsRichTextBox.Size = new System.Drawing.Size(481, 217);
+            this.GuestsRichTextBox.TabIndex = 0;
+            this.GuestsRichTextBox.Text = "";
+            // 
+            // AddGuestButton
+            // 
+            this.AddGuestButton.Location = new System.Drawing.Point(12, 235);
+            this.AddGuestButton.Name = "AddGuestButton";
+            this.AddGuestButton.Size = new System.Drawing.Size(75, 23);
+            this.AddGuestButton.TabIndex = 1;
+            this.AddGuestButton.Text = "Add guest";
+            this.AddGuestButton.UseVisualStyleBackColor = true;
+            // 
+            // RejectButton
+            // 
+            this.RejectButton.Location = new System.Drawing.Point(142, 235);
+            this.RejectButton.Name = "RejectButton";
+            this.RejectButton.Size = new System.Drawing.Size(75, 23);
+            this.RejectButton.TabIndex = 2;
+            this.RejectButton.Text = "Reject guest";
+            this.RejectButton.UseVisualStyleBackColor = true;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(275, 235);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(75, 23);
+            this.ExportButton.TabIndex = 3;
+            this.ExportButton.Text = "Export to txt";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(418, 235);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 4;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(512, 269);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.RejectButton);
+            this.Controls.Add(this.AddGuestButton);
+            this.Controls.Add(this.GuestsRichTextBox);
+            this.Name = "Form1";
+            this.Text = "Hotel";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox GuestsRichTextBox;
+        private System.Windows.Forms.Button AddGuestButton;
+        private System.Windows.Forms.Button RejectButton;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Button CloseButton;
     }
 }
 
