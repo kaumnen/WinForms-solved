6e378fa449dad2294a2656102c3b27bb4257b9fe
Aleksej Komnenovic <main@komnen.xyz>
2021-04-13 21:19:01 +0200

    add data model class

---
+++ BOOK/4.2/WinFormsSolution/Exercise 4.2/AdModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4._2
+{
+    class AdModel
+    {
+        public string Position { get; private set; }
+        public int Yoe { get; private set; }
+        public string AcademicLevel { get; private set; }
+
+        public AdModel(string position, int YOE, string academicLevel)
+        {
+            Position = position;
+            Yoe = YOE;
+            AcademicLevel = academicLevel;
+        }
+
+        public override string ToString()
+        {
+            return $"Position: {Position} - YOE: {Yoe} - Academic level: {AcademicLevel}\r\n";
+        }
+    }
+}
